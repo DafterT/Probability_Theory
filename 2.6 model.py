076ec1ba136ebab16154676b3c8d4ddc38428cf9
Dafter <DafterPlay@mail.ru>
2023-03-16 18:56:13 +0300

    add 2.6 model

---
+++ 2.6 model.py
@@ -0,0 +1,41 @@
+from random import randint
+
+
+def get_random_coin(count_20, count_3):
+    x = randint(1, count_3 + count_20)
+    if x <= count_20:
+        return 20
+    return 3
+
+
+def one_iteration():
+    count_20 = 3
+    count_3 = 7
+    first_coin = get_random_coin(count_20, count_3)
+    if first_coin == 20:
+        count_20 -= 1
+    else:
+        count_3 -= 1
+    second_coin = get_random_coin(count_20, count_3)
+    if second_coin != 20:
+        return
+    return 1 if first_coin == 20 else 0
+
+
+def main():
+    iteration_counter = 0
+    event_counter = 0
+    while iteration_counter < 1_000_000:
+        iteration = one_iteration()
+        if iteration is None:
+            continue
+        event_counter += iteration
+        iteration_counter += 1
+    print(f'Количество попыток, когда второй раз выпала 20: {iteration_counter}\n'
+          f'Количество выпадений двух 20 подряд: {event_counter}\n'
+          f'Итоговая вероятность: {event_counter / iteration_counter}\n'
+          f'Ожидаемая вероятность: {2 / 9}')
+
+
+if __name__ == '__main__':
+    main()
