dec46b0a2815853ba25a0cea988bf119414a999a
Dafter <DafterPlay@mail.ru>
2023-03-17 15:26:56 +0300

    add redo input in 2.6 model

--- 2.6 model.py
+++ 2.6 model.py
@@ -1,6 +1,12 @@
 from random import randint
 
 
+def get_input_data():
+    count_20 = 3
+    count_3 = 7
+    return count_20, count_3
+
+
 def get_random_coin(count_20, count_3):
     """
     Получение результата доставания монетки
@@ -11,12 +17,10 @@ def get_random_coin(count_20, count_3):
     return 3
 
 
-def one_iteration():
+def one_iteration(count_20, count_3):
     """
     Одна итерация вытягивания двух монет
     """
-    count_20 = 3
-    count_3 = 7
     first_coin = get_random_coin(count_20, count_3)
     if first_coin == 20:
         count_20 -= 1
@@ -32,18 +36,20 @@ def main():
     """
     Основной цикл, запускающий одну итерацию несколько раз
     """
+    count_20, count_3 = get_input_data()
     iteration_counter = 0
     event_counter = 0
     while iteration_counter < 1_000_000:
-        iteration = one_iteration()
+        iteration = one_iteration(count_20, count_3)
         if iteration is None:
             continue
         event_counter += iteration
         iteration_counter += 1
-    print(f'Количество попыток, когда второй раз выпала 20: {iteration_counter}\n'
+    print(f'Количество 20-ок: {count_20}, количество 3-ек: {count_3}\n'
+          f'Количество попыток, когда второй раз выпала 20: {iteration_counter}\n'
           f'Количество выпадений двух 20 подряд: {event_counter}\n'
           f'Итоговая вероятность: {event_counter / iteration_counter}\n'
-          f'Ожидаемая вероятность: {2 / 9}')
+          f'Ожидаемая вероятность: {(count_20 - 1) / (count_20 + count_3 - 1)}')
 
 
 if __name__ == '__main__':
